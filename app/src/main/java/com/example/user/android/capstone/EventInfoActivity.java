8177d2596d464297b05248107cddd1f0e7c659f7
natalia-ku <bilberryfm@mail.ru>
2017-07-04 18:50:16 -0700

    map get direction works now

--- app/src/main/java/com/example/user/android/capstone/EventInfoActivity.java
+++ app/src/main/java/com/example/user/android/capstone/EventInfoActivity.java
@@ -1,7 +1,12 @@
 package com.example.user.android.capstone;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +26,7 @@ public class EventInfoActivity extends AppCompatActivity {
     TextView mEventInfoDetails;
     TextView mEventInfoPeopleNeeded;
     TextView mEventInfoCreatorId;
+    Button mGetDirectionsButton;
 
     DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
     DatabaseReference mEventsRef = mRootRef.child("events");
@@ -29,6 +35,40 @@ public class EventInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
+
+        mEventInfoSportType = (TextView) findViewById(R.id.event_sporttype_textview);
+        mEventInfoAddress = (TextView) findViewById(R.id.event_address_textview);
+        mEventInfoDate = (TextView) findViewById(R.id.event_date_textview);
+        mEventInfoTime = (TextView) findViewById(R.id.event_time_textview);
+        mEventInfoDetails = (TextView) findViewById(R.id.event_details_textview);
+        mEventInfoPeopleNeeded = (TextView) findViewById(R.id.event_people_needed_textview);
+        mEventInfoCreatorId = (TextView) findViewById(R.id.event_creator_id_textview);
+        mGetDirectionsButton = (Button) findViewById(R.id.get_directions_button);
+
+        getEventInfo();
+
+        mGetDirectionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String addressString = mEventInfoAddress.getText().toString();
+                Uri geoLocation = Uri.parse("geo:0,0?q=" + addressString);
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                } else {
+                    System.out.println("Couldn't call " + geoLocation.toString()
+                            + ", no receiving apps installed!");
+                }
+            }
+        });
+
+
+
+    }
+
+
+    private void getEventInfo(){
         String eventId = getIntent().getStringExtra("id");
         Query eventDetailsQuery = mEventsRef.orderByKey().equalTo(eventId);
 
@@ -39,10 +79,8 @@ public class EventInfoActivity extends AppCompatActivity {
 
                     for (DataSnapshot eventSnapshot : dataSnapshot.getChildren()) {
                         String address = (String) eventSnapshot.child("address").getValue();
-
                         String date = (String) eventSnapshot.child("dataTime").getValue();
                         String time = (String) eventSnapshot.child("time").getValue();
-
                         String creatorId = eventSnapshot.child("creatorId").getValue().toString();
                         String details = (String) eventSnapshot.child("details").getValue();
                         String peopleNeeded = eventSnapshot.child("peopleNeeded").getValue().toString();
@@ -65,14 +103,6 @@ public class EventInfoActivity extends AppCompatActivity {
 
             }
         });
-
-        mEventInfoSportType = (TextView) findViewById(R.id.event_sporttype_textview);
-        mEventInfoAddress = (TextView) findViewById(R.id.event_address_textview);
-        mEventInfoDate = (TextView) findViewById(R.id.event_date_textview);
-        mEventInfoTime = (TextView) findViewById(R.id.event_time_textview);
-        mEventInfoDetails = (TextView) findViewById(R.id.event_details_textview);
-        mEventInfoPeopleNeeded = (TextView) findViewById(R.id.event_people_needed_textview);
-        mEventInfoCreatorId = (TextView) findViewById(R.id.event_creator_id_textview);
-
     }
+
 }
